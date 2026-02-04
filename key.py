99c310d18550b7ec9b7fecf61bbfe6c6a3a119fd
jiabingxian <2453650866@qq.com>
2026-02-04 20:02:18 +0800

    重构

--- key.py
+++
@@ -1,5 +0,0 @@
-from data import if_key
-while True:
-    value=input('请输入二维码：')
-    a=if_key(value,True)
-    print(a)
---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+import base64
+import datetime
+class Card:
+    def __init__(self,name:str,id:int,y:int,m:int,d:int):
+        self.name=name
+        self.y=y
+        self.m=m
+        self.d=d
+        self.id=id
+    def calc(self):
+        a=int(self.id) * 2 // 5
+        if int(self.id)*2%5!=0: # 计算校验码
+            a*=2
+            if a >=10000:
+                a=a//5
+            else:
+                a=a+10
+        else:
+            a=a//2*10
+        self.verifyid=a
+        self.cida=a*2
+    def encode(self):
+        self.calc()
+        n = str(self.name) + ',' + str(self.id) + ',' + str(self.verifyid) + ',' + str(self.cida) + ',' + str(self.y) + ',' + str(self.m) + ',' + str(self.d)
+        n = n.encode('utf-8')
+        n = base64.b64encode(n)
+        n=str(n, 'utf-8')
+        return n
+    def __str__(self):
+        return self.encode()
+    def info(self):
+        return f"姓名:{self.name}\n卡号:{self.id}\n校验码:{self.verifyid}\nCIDA:{self.cida}\n有效期至:{self.y}年{self.m}月{self.d}日"
+    def verify(self):
+        a = int(self.id) * 2 // 5
+        if int(self.id)*2%5!=0: # 计算校验码
+            a*=2
+            if a >=10000:
+                a=a//5
+            else:
+                a=a+10
+        else:
+            a=a//2*10
+        if self.cida // 2 == self.verifyid and self.verifyid == a:
+            return True
+        else:
+            return False
